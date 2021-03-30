eace735aefdaf6e51c6e0df94610a55775e5430a
marjacob <marjacob@users.noreply.github.com>
2021-03-30 11:44:23 +0200

    Disable layer formatting

--- keyboards/hhkb/ansi/keymaps/marjacob/keymap.c
+++ keyboards/hhkb/ansi/keymaps/marjacob/keymap.c
@@ -4,6 +4,7 @@
 #include "marjacob.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /* clang-format off */
 
     [_BASE] = LAYOUT(
         KC_ESC , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , KC_BSLS, KC_GRV,
@@ -45,5 +46,5 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                             _______, _______, _______, _______, _______
     )
 
+    /* clang-format on */
 };
-
